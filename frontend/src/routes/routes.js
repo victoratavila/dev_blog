2f067da1bddab39f9db278e21f701a5a9fa2c6af
Victor Atavila <victoratavila@hotmail.com>
2020-12-23 23:36:14 -0300

    Articles view

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -19,7 +19,29 @@ router.get('/categorias', (req, res) => {
 
 router.get('/categorias/:slug', (req, res) => {
     const { slug } = req.params;
-    res.render('articlesByCategory.ejs', { slug: slug})
+
+    axios.get(baseURL + '/category/' + slug).then((category) => {
+        if(category){
+
+            axios.get(baseURL + '/article/' + slug).then(articles => {
+                if(articles.data.error != true ){
+                    console.log(articles)
+                    const articlesNotFound = '';
+                    res.render('articlesByCategory.ejs', { slug: slug, categoryName: category.data, articlesNotFound: articlesNotFound, articles: articles.data })
+                } else {
+                    const articlesNotFound = 'Parece que essa categoria ainda não possui nenhum artigo cadastrado, seja o primeiro a publicar!';
+                    res.render('articlesByCategory.ejs', { slug: slug, categoryName: category.data, articlesNotFound: articlesNotFound, articles: null })
+                }
+            }).catch(err => {
+                console.log(err);
+            })
+            
+        } else {
+            res.redirect('/categorias')
+        }
+    }).catch(err => {
+        console.log(err);
+    })
 
 })
 
