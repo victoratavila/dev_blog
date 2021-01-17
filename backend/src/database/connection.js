305eaa1fe8c0da058c55389f79727d134db63909
Victor Atavila <victoratavila@hotmail.com>
2021-01-17 00:35:49 -0300

    Deploy config

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('dev_blog', 'root', process.env.DB_PASSWORD, {
+const sequelize = new Sequelize('dev_blog', 'devblogDB', process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     logging: false,
